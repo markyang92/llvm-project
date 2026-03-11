ea5bb841709bbb82d308579360ea638a79e53691
Andrei Elovikov <andrei.elovikov@sifive.com>
2026-03-11 09:55:59 -0700

    Drop leftover comment

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -7867,7 +7867,6 @@ bool VPRecipeBuilder::replaceWithFinalIfReductionStore(
           SI, VPI->operandsWithoutMask(), true /* IsUniform */,
           nullptr /*Mask*/, *VPI, *VPI, VPI->getDebugLoc());
       FinalRedStoresBuilder.insert(Recipe);
-      // Recipe->insertBefore(*MiddleVPBB, MBIP);
     }
     VPI->eraseFromParent();
     return true;
